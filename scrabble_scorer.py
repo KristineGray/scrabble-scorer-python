19ff75afee54c3170a3e62926a87cb1d1492dda3
KristineGray <krisgraya@gmail.com>
2021-08-17 17:21:01 -0400

    refactored initial_prompt & old_scrabble_scorer methods

--- scrabble_scorer.py
+++ scrabble_scorer.py
@@ -2,37 +2,36 @@
 
 
 OLD_POINT_STRUCTURE = {
-  1: ['A', 'E', 'I', 'O', 'U', 'L', 'N', 'R', 'S', 'T'],
-  2: ['D', 'G'],
-  3: ['B', 'C', 'M', 'P'],
-  4: ['F', 'H', 'V', 'W', 'Y'],
-  5: ['K'],
-  8: ['J', 'X'],
-  10: ['Q', 'Z']
+    1: ['A', 'E', 'I', 'O', 'U', 'L', 'N', 'R', 'S', 'T'],
+    2: ['D', 'G'],
+    3: ['B', 'C', 'M', 'P'],
+    4: ['F', 'H', 'V', 'W', 'Y'],
+    5: ['K'],
+    8: ['J', 'X'],
+    10: ['Q', 'Z']
 }
 
 def old_scrabble_scorer(word):
     word = word.upper()
-    letterPoints = ""
+    letter_points = ""
 
     for char in word:
+        for point_value in OLD_POINT_STRUCTURE:
+            if char in OLD_POINT_STRUCTURE[point_value]:
+                letter_points += f'Points for {char}: {point_value}\n'
 
-        for point_value in old_point_structure:
-
-            if char in old_point_structure[point_value]:
-                letterPoints += 'Points for {char}: {point_value}\n'.format(char = char, point_value = point_value)
-
-    return letterPoints
+    return letter_points
 
 # your job is to finish writing these functions and variables that we've named
 # don't change the names or your program won't work as expected.
 
 def initial_prompt():
-   print("Let's play some Scrabble!\n")
-
+    print("Let's play some Scrabble!\n")
+    word = input("\nEnter a word to score: ")
+    return word
 
 def simple_scorer():
-   return 
+    return 
 
 def vowel_bonus_scorer():
     return 
@@ -50,3 +49,5 @@ def transform():
 
 def run_program():
     word = initial_prompt()
+    letter_points = old_scrabble_scorer(word)
+    print(letter_points)
